5e7e43a72de48c1aa9d49f3f27ca6e867388b7b3
Destan Tezel <destantezel@googlemail.com>
2023-12-05 16:34:33 +0000

    Learn conditional rendering

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,12 +3,14 @@ import Sidebar from './components/Sidebar';
 import NavBarSimple from './components/NavBarSimple';
 import EventsFunctional from './components/EventsFunctional';
 import EventsClass from './components/EventsClass';
+import ConditionalRenderingClass from './components/ConditionalRenderingClass';
+import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
 
 function App() {
   return (
     <div className={css.App}>
-      <EventsFunctional />
-      <EventsClass />
+      <ConditionalRenderingClass />
+      <ConditionalRenderingFunctional connected={true} />
     </div>
   );
 }
