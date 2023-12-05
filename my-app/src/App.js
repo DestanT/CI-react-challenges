f0a9c9a6314e9566fcc6afebb5a12b703804c657
Destan Tezel <destantezel@googlemail.com>
2023-12-05 16:08:58 +0000

    Learn about ES7 React shorcuts and create event handlers in class and functional components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import css from './App.module.css';
 import Sidebar from './components/Sidebar';
 import NavBarSimple from './components/NavBarSimple';
+import EventsFunctional from './components/EventsFunctional';
+import EventsClass from './components/EventsClass';
 
 function App() {
   return (
     <div className={css.App}>
-      <Sidebar />
-      <NavBarSimple />
+      <EventsFunctional />
+      <EventsClass />
     </div>
   );
 }
