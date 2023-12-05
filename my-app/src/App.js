d26e4b93bf0b3458bcc86e4719cd3f60fe1fdf4a
Destan Tezel <destantezel@googlemail.com>
2023-12-05 17:03:37 +0000

    Learn about methods as props - from parent to child

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,13 +6,14 @@ import EventsClass from './components/EventsClass';
 import ConditionalRenderingClass from './components/ConditionalRenderingClass';
 import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
 import NestingComponents from './components/NestingComponents';
+import MethodsAsPropsParent from './components/MethodsAsPropsParent';
 
 function App() {
-  return (
-    <div className={css.App}>
-      <NestingComponents />
-    </div>
-  );
+    return (
+        <div className={css.App}>
+            <MethodsAsPropsParent />
+        </div>
+    );
 }
 
 export default App;
