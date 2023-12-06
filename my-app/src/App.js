cf04a6b14ff0ce458fc93f2f8c12e623c6b73310
Destan Tezel <destantezel@googlemail.com>
2023-12-06 15:19:57 +0000

    Learn how to render React components by passing array objects as props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,11 +8,14 @@ import ConditionalRenderingClass from './components/ConditionalRenderingClass';
 import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
 import NestingComponents from './components/NestingComponents';
 import MethodsAsPropsParent from './components/MethodsAsPropsParent';
+import RenderingLists from './components/RenderingLists';
 
 function App() {
     return (
         <div className={css.App}>
+          <Sidebar />
           <NavBarForm />
+          <RenderingLists />
         </div>
     );
 }
